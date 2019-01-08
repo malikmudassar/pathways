1661ebe9e558229e311db3e8358815126d181bfa
Mudassar <mudassar.khani@attech-ltd.com>
2019-01-08 17:42:07 +0500

    controller

--- application/controllers/api/Pw.php
+++ application/controllers/api/Pw.php
@@ -161,7 +161,7 @@ class Pw extends REST_Controller {
         $data['answer']=$this->Admin_model->getStepAnswer($params); 
         if(!$data['answer'])
         {
-            $data['answer']="";
+            $data['answer']=array();
         }
         $data['form']=$this->Admin_model->getAnsForm($data['question']['id']);
 
