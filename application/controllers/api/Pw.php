c861fd71a4ce341d7441ada4cf376a3a10b9c949
Mudassar <mudassar.khani@attech-ltd.com>
2019-02-07 17:40:11 +0500

    changes

--- application/controllers/api/Pw.php
+++ application/controllers/api/Pw.php
@@ -339,7 +339,8 @@ class Pw extends REST_Controller {
         $params=$_REQUEST;
         $data=$this->Admin_model->getEditedQuestion($params);
         $data['form']=$this->Admin_model->getAnsForm($data['question']['id'],$params);
-        $data['answer']=$this->Admin_model->getStepAnswer($params);
+        $data['answer']=array();
+        $data['answer'][0]=$this->Admin_model->getStepAnswer($params);
         $path=$this->Admin_model->getPathFlowByStep($params['step'],$params['pathway']);
         $data['step']=$path['step'];
         $data['back']=$path['back'];
