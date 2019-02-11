8c30dbf7f418202beeb5b35ff62dc42f8208301e
Mudassar <mudassar.khani@attech-ltd.com>
2019-02-11 22:00:33 +0500

    changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -92,7 +92,7 @@ class Admin_model extends CI_Model {
         }
         if($data['pathway']==3 && $data['step']==3)
         {
-            $data['step']=1;
+            $data['step']="1";
             $step=$this->getStepByNumberPathway($data['step'],$id);
             $data['percent']=0;
         }
