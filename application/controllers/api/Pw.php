55c4c585deb5906d45c7d49ebb57888898a323ac
Mudassar <mudassar.khani@attech-ltd.com>
2019-02-12 01:18:40 +0500

    changes

--- application/controllers/api/Pw.php
+++ application/controllers/api/Pw.php
@@ -293,7 +293,7 @@ class Pw extends REST_Controller {
         else
         {
             $data['step_type']='info';
-            $data['form'][0]=[];
+            $data['form'][0]=array();
         }
         if($data['back']==0)
         {
