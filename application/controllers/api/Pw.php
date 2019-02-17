324e3ec31de6f81a1cd9edcd90b37e4802fc6bf9
mudassar <malikmudassar@gmail.com>
2019-02-17 16:49:57 +0500

    changes

--- application/controllers/api/Pw.php
+++ application/controllers/api/Pw.php
@@ -69,9 +69,10 @@ class Pw extends REST_Controller {
     {
         $this->session->set_userdata('flag','white');
         $Id=$_REQUEST['pw'];
+        $params['pathway']=$Id;
         
         $data=$this->Admin_model->getFirstPathwayQuestion($Id);
-        $data['form']=$this->Admin_model->getAnsForm($data['question']['id'], $_REQUEST);
+        $data['form']=$this->Admin_model->getAnsForm($data['question']['id'], $params);
 
         if(!empty($data['form']))
         {
