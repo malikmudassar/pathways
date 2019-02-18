8f2917b714feaf85b8a1930968a8fe752d04b7e7
Mudassar <mudassar.khani@attech-ltd.com>
2019-02-18 17:55:53 +0500

    changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -3097,7 +3097,7 @@ class Admin_model extends CI_Model {
                 $st=$this->db->query('select questions.* from questions inner join step_questions on step_questions.question=questions.id where step='.$step['id'])->result_array();
                 // echo $this->db->last_query();
                 $data['question']=$st[0];
-                $data['question']['statement']= 'Your BMI is '.number_format($result,2).'. Your BMI category is '.$category;
+                $data['question']['statement']= 'Your BMI is '.number_format($result,2).'. Your result suggests you are '.$category;
                 return $data;
             }
         }
