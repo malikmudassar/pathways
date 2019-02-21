341931b2a84d9329073de610d7a92ffeece135d4
Mudassar <mudassar.khani@attech-ltd.com>
2019-02-21 12:40:27 +0500

    changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -771,7 +771,14 @@ class Admin_model extends CI_Model {
                 $weight=$result[0]['value'];
                 $height=$result[1]['value'];
                 $result=array();
-                $result['value']=(($weight)/(($height*$height)/10000));
+                $t=$this->db->select('*')->from('step_answers')
+                            ->where('user_id', $params['user_id'])
+                            ->where('pathway', $params['pathway'])
+                            ->where('step', 2)
+                            ->get()
+                            ->result_array();
+                $result['value']=$t[0]['value'];
+                // $result['value']=(($weight)/(($height*$height)/10000));
                 // echo "<script>console.log('653. Result is '".$result['value'].")</script>";
             }
             else
