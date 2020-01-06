728b44cdae0a7ec910bb26d1a7b84defb3954b93
Mudassar <mudassar.khani@attech-ltd.com>
2020-01-06 17:59:02 +0500

    changes in model

--- application/controllers/api/Pw.php
+++ application/controllers/api/Pw.php
@@ -436,6 +436,10 @@ class Pw extends REST_Controller {
         {
             $data['answer']=array_reverse($data['answer']);
         }
+        if($data['pathway']==21 && $data['step']==22)
+        {
+            $data['answer']=array_reverse($data['answer']);
+        }
         $this->Admin_model->removeFlowStep($step['number'], $params['pathway'], $params['user_id']);
         ///////////////////////////////////////////////////////////////
         if ($data)
