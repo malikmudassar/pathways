492d63e77e4a51f2456fff7d5fdfc3090a69a8cc
Mudassar <mudassar.khani@attech-ltd.com>
2020-01-08 00:21:24 +0500

    changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -7826,6 +7826,14 @@ class Admin_model extends CI_Model {
         $row=$this->db->query('select * from step_answers where step='.$step.' and pathway='.$params['pathway'].' and user_id='.$params['user_id'])->result_array();
         // print_r($row);
         // check if there are more than one answers 
+        if($params['pathway']==21 && $step==22)
+        {
+            $d=array();
+            $d[0]=$row[1];
+            $d[1]=$row[2];
+            $d[2]=$row[0];
+            $row=$d;
+        }
         if(count($row)>1)
         {
             // More than one answers, either they are text boxes or a mixed answer model
