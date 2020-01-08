814f4d136bd1cb5c1a08ecb91517b78f3bd40b3b
Mudassar <mudassar.khani@attech-ltd.com>
2020-01-08 15:47:12 +0500

    changes in fucking controller

--- application/controllers/api/Pw.php
+++ application/controllers/api/Pw.php
@@ -644,6 +644,7 @@ class Pw extends REST_Controller {
         $data2['code']='200';
         $data2['message']='Your request for online consultation has been submitted successfully';
         $data2['slotId']=$slot->slot_id;
+        $this->Admin_model->finish_pw($params['pathway'], $params['user_id']);
         if($data2)
         {
             // Set the response and exit
