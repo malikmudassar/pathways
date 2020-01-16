3477f5692711264cd7c3422c642fc86841583dc9
Mudassar <mudassar.khani@attech-ltd.com>
2020-01-16 20:09:05 +0500

    changes

--- application/controllers/api/Pw.php
+++ application/controllers/api/Pw.php
@@ -691,7 +691,7 @@ class Pw extends REST_Controller {
         $data2['code']='200';
         $data2['message']='Your request for online consultation has been submitted successfully';
         // $data2['slotId']=$this->Admin_model->getLastInsertedSlotId($data);
-        $data2['slotId']=($slot->slot_id)?$slot->slot_id:4813;
+        $data2['slotId']=($slot->slot_id)?$slot->slot_id:3955;
         $this->Admin_model->finish_pw($params['pathway'], $params['user_id']);
         if($data2)
         {
